f491e49a66fe313412e8837f299aa1c0f14f9e5b
anupsinghh <itsanupsingh018@gmail.com>
2025-07-08 15:24:43 +0530

    updated backend link

--- src/pages/ComplaintForm.jsx
+++ src/pages/ComplaintForm.jsx
@@ -33,7 +33,7 @@ const ComplaintForm = () => {
     };
 
     try {
-      const res = await fetch('http://localhost:5000/api/complaints', {
+      const res = await fetch('https://fixit-backend.onrender.com/api/complaints', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
